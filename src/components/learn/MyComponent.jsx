271eda2a6d709ceb591bd2fe3bf3d682fb9a94ae
GaHongHacDev <tientranxuan263@gmail.com>
2025-06-10 17:16:03 +0700

    video 33

--- src/components/learn/MyComponent.jsx
+++ src/components/learn/MyComponent.jsx
@@ -1,10 +1,18 @@
 import './style.css';
 
 const MyComponent = () => {
+    const hoidanit = 'tien'; //string
     return (
         <>
-            <div>Tien tran xuan - dep trai</div>
-            <div className="child">child</div>
+            <div>{hoidanit} tran xuan - dep trai</div>
+            <div className="child" style={
+                {
+                    color: 'red',
+                    backgroundColor: 'black',
+                    padding: '10px',
+                    borderRadius: '5px'
+                }
+            }>child</div>
         </>
     );
 }
